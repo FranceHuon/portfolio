c65149a53699358cea69ce70a9124548ea285512
FranceHuon <133089058+FranceHuon@users.noreply.github.com>
2023-11-29 09:23:12 +0100

    Réorganisation de la page contact

--- src/components/ContactComponent.jsx
+++ src/components/ContactComponent.jsx
@@ -8,28 +8,60 @@ function Columns() {
         <Container>
             <Row>
                 <Col className='key'>
-                    <div id='placeKey'><h2 className="custom-h2">LIEU</h2></div>
-                    <div className="ligne-horizontale"></div>
-                    <div id='phoneKey'><h2 className="custom-h2">EMAIL</h2></div>
-                    <div className="ligne-horizontale"></div>
-                    <div id='emailKey'><h2 className="custom-h2">TÉLÉPHONE</h2></div>
-                    <div className="ligne-horizontale"></div>
-                    <div id='linkedinKey'><h2 className="custom-h2">LINKEDIN</h2></div>
-                    <div className="ligne-horizontale"></div>
-                    <div id='GitHubKey'><h2 className="custom-h2">GITHUB</h2></div>
+                    <div id='placeKey'><h2 className="custom-h2 ligne-horizontale">LIEU</h2></div>
+                    {/* <div className="ligne-horizontale"></div> */}
+                    <div id='phoneKey'><h2 className="custom-h2 ligne-horizontale">EMAIL</h2></div>
+                   
+                    <div id='emailKey'><h2 className="custom-h2 ligne-horizontale">TÉLÉPHONE</h2></div>
+                    
+                    <div id='linkedinKey'><h2 className="custom-h2 ligne-horizontale">LINKEDIN</h2></div>
+                    
+                    <div id='GitHubKey'><h2 className="custom-h2 ligne-horizontale">GITHUB</h2></div>
                 </Col>
                 <Col className='value'>
-                    <div id='placeValue'><h2 className="custom-h2">NANTES</h2></div>
-                    <div className="ligne-horizontale"></div>
-                    <div id='phoneValue'><h2 className="custom-h2">06 09 36 12 96</h2></div>
-                    <div className="ligne-horizontale"></div>
-                    <div id='emailValue'><h2 className="custom-h2">FRANCEHUON@GMAIL.COM</h2></div>
-                    <div className="ligne-horizontale"></div>
-                    <div id='linkedinValue'><h2 className="custom-h2">LINKEDIN/IN/FRANCEHUON</h2></div>
-                    <div className="ligne-horizontale"></div>
-                    <div id='GitHubValue'><h2 className="custom-h2">GITHUB/FRANCEHUON</h2></div>
+                    <div id='placeValue'><h2 className="custom-h2 ligne-horizontale">NANTES</h2></div>
+                   
+                    <div id='emailValue'><h2 className="custom-h2 ligne-horizontale">FRANCEHUON@GMAIL.COM</h2></div>
+                    
+                    <div id='phoneValue' className="ligne-horizontale"><h2 className="custom-h2 ligne-horizontale">06 09 36 12 96</h2></div>
+                    
+                    <div id='linkedinValue'className="ligne-horizontale"><h2 className="custom-h2 ligne-horizontale">LINKEDIN/IN/FRANCEHUON</h2></div>
+                  
+                    <div id='GitHubValue' className="ligne-horizontale"><h2 className="custom-h2 ">GITHUB/FRANCEHUON</h2></div>
                 </Col>
             </Row>
+            <dl class="row">
+                <dt class="col-2">
+                Lieu
+                </dt>
+                <dd class="col-10">
+                Nantes
+                </dd>
+                <dt class="col-2">
+                Email
+                </dt>
+                <dd class="col-10">
+                francehuon@gmail.com
+                </dd>
+                <dt class="col-2">
+                Téléphone
+                </dt>
+                <dd class="col-10">
+                06 09 36 12 96
+                </dd>
+                <dt class="col-2">
+                Linkedin
+                </dt>
+                <dd class="col-10">
+                LINKEDIN/IN/FRANCEHUON
+                </dd>
+                <dt class="col-2">
+                    GitHub
+                </dt>
+                <dd class="col-10">
+                GITHUB/FRANCEHUON
+                </dd>
+            </dl>
         </Container>
     );
 }
